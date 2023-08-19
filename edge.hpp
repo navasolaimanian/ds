ae2097281e918642697ba9c97a5ebd163c40d212
elmiraghadiri <aghdyryfrd@gmail.com>
2023-08-19 19:25:53 +0330

    add more info in edge for saving galaxy 1 and 2 id and name and get cost

--- edge.hpp
+++ edge.hpp
@@ -8,8 +8,12 @@ private:
     int node1Id; // The ID of the first node connected by the edge
     int node2Id; // The ID of the second node connected by the edge
     int weight;  // The weight/cost of the edge
+    int galaxy1Id;
+    int galaxy2Id;
+    std::string node1Name;
+    std::string node2Name;
 public:
-    Edge(int, int, int); // cunstructor
+    Edge(int , int , int , int, int, std::string, std::string); // cunstructor
 
     int getNode1ID(); // get id of first node
 
@@ -18,6 +22,20 @@ public:
     int getNode2ID(); // get id of second node
 
     void setNode2ID(int); // set id of second node
+
+    int getCost();
+
+    void setCost(int);
+
+    int getGalaxy1Id() { return galaxy1Id; };
+
+    int getGalaxy2Id() { return galaxy2Id; };
+
+    std::string getNode1Name() { return node1Name; };
+    
+    std::string getNode2Name() { return node2Name; };
+
+
 };
 
 #endif
